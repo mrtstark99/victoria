4689a80660917d242b51295adc18f372a2cb2e27
BrightHome Dev <dev@brighthome.vn>
2026-08-10 00:51:03 +0900

    Deploy Victoria Universal website

---
+++ worker.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+
+    if (response.status !== 404) {
+      return response;
+    }
+
+    const url = new URL(request.url);
+    if (!url.pathname.split('/').pop()?.includes('.')) {
+      return env.ASSETS.fetch(new Request(new URL('/index.html', url), request));
+    }
+
+    return response;
+  }
+};
